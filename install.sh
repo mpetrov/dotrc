2a60ca02246c820d358d23804975f31b4b650750
Martin Petrov <martin@martinpetrov.com>
2011-11-02 11:44:38 -0400

    added os x scripts

--- install.sh
+++ install.sh
@@ -19,10 +19,6 @@ mv -f ${ZSHRC} ${BACKUP} 2> /dev/null
 mv -f ${DOTVIM} ${BACKUP} 2> /dev/null
 mv -f ${SCREENRC} ${BACKUP} 2> /dev/null
 
-ln -s ${PREFIX}/submodules/vim-irblack/colors/ir_black.vim ${PREFIX}/vim/colors/ir_black.vim   || echo "symlink exists"
-ln -s ${PREFIX}/submodules/vim-pathogen/autoload ${PREFIX}/vim/  || echo "symlink exists"
-
-
 ln -s ${PREFIX}/rc.vim ${VIMRC}
 ln -s ${PREFIX}/vim ${DOTVIM} 
 ln -s ${PREFIX}/rc.zsh ${ZSHRC} 
---
+++ osx/packages.sh
@@ -0,0 +1,5 @@
+brew install git dos2unix unrar wget unix2dos par git-extras
+brew install --HEAD https://raw.github.com/adamv/homebrew-alt/master/duplicates/vim.rb
+brew install --HEAD https://raw.github.com/adamv/homebrew-alt/master/duplicates/screen.rb
+
+
