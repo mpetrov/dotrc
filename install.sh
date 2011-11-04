66e4dad2ac02794b3fe8bbc3f51f5ff847fe6503
Martin Petrov <martin@martinpetrov.com>
2011-11-04 14:24:15 -0400

    rolled back some changes

--- install.sh
+++ install.sh
@@ -19,6 +19,11 @@ mv -f ${ZSHRC} ${BACKUP} 2> /dev/null
 mv -f ${DOTVIM} ${BACKUP} 2> /dev/null
 mv -f ${SCREENRC} ${BACKUP} 2> /dev/null
 
+ln -s ${PREFIX}/submodules/vim-command-t ${PREFIX}/vim/bundle/  || echo "symlink exists"
+ln -s ${PREFIX}/submodules/vim-supertab ${PREFIX}/vim/bundle/  || echo "symlink exists"
+ln -s ${PREFIX}/submodules/vim-pathogen/autoload ${PREFIX}/vim/  || echo "symlink exists"
+
+
 ln -s ${PREFIX}/rc.vim ${VIMRC}
 ln -s ${PREFIX}/vim ${DOTVIM} 
 ln -s ${PREFIX}/rc.zsh ${ZSHRC} 
