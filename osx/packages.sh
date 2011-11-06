3a6b04cfc9048d6ddfc30d1969d18974cfe454de
Martin Petrov <martin@martinpetrov.com>
2011-11-06 11:27:51 -0500

    added duti to os x binaries

--- osx/packages.sh
+++ osx/packages.sh
@@ -1,4 +1,6 @@
-brew install git dos2unix unrar wget unix2dos par git-extras
+#!/bin/sh
+easy_install mercurial
+brew install -f  dos2unix unrar wget unix2dos par git-extras
 brew install --HEAD https://raw.github.com/adamv/homebrew-alt/master/duplicates/vim.rb
 brew install --HEAD https://raw.github.com/adamv/homebrew-alt/master/duplicates/screen.rb
 
