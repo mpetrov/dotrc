579a6028369d00d1cb3cf8a9f97240427d0142ae
Martin Petrov <martin@martinpetrov.com>
2012-04-10 11:17:28 -0400

    added a reload env vars

--- rc.zsh
+++ rc.zsh
@@ -30,3 +30,12 @@ if [[ "$unamestr" == 'Linux' ]]; then
 elif [[ "$unamestr" == 'Darwin' ]]; then
 fi
 
+function r() {   
+  if [[ -n $TMUX ]]; then
+    NEW_SSH_AUTH_SOCK=`tmux showenv|grep ^SSH_AUTH_SOCK|cut -d = -f 2`
+    if [[ -n $NEW_SSH_AUTH_SOCK ]] && [[ -S $NEW_SSH_AUTH_SOCK ]]; then 
+      SSH_AUTH_SOCK=$NEW_SSH_AUTH_SOCK  
+    fi
+  fi
+}
+
